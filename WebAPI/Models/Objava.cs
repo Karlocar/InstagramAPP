524595a3b14ec75321e230dcc8182ca6df6b5940
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-10-01 01:03:50 +0200

    Create Objava.cs

---
+++ WebAPI/Models/Objava.cs
@@ -0,0 +1,17 @@
+﻿namespace WebApi.Models
+{
+    public class Objava : Entitet
+    {
+        public string? Naslov { get; set; }
+        
+        public string?  Opis { get; set; }
+
+        public DateTime? VrijemeIzrade { get; set; }
+
+        public string? IpAdresa { get; set; }
+
+        public int? Osoba { get; set; } 
+        public string? Slika { get; set; }
+
+    }
+}
