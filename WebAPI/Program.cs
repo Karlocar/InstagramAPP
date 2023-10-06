b7fe67b3abeba1f58b46daa4e739c63c6d1bef95
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-10-06 02:54:29 +0200

    Update Program.cs

--- WebAPI/Program.cs
+++ WebAPI/Program.cs
@@ -1,15 +1,47 @@
-using Instagram.Data;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
+using System.Reflection;
+using WebApi.Data;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
+
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
+builder.Services.AddSwaggerGen(sgo => { // sgo je instanca klase SwaggerGenOptions
+    // èitati https://devintxcontent.blob.core.windows.net/showcontent/Speaker%20Presentations%20Fall%202017/Web%20API%20Best%20Practices.pdf
+    var o = new Microsoft.OpenApi.Models.OpenApiInfo()
+    {
+        Title = "Instagram API",
+        Version = "v1",
+        Contact = new Microsoft.OpenApi.Models.OpenApiContact()
+        {
+            Email = "karlocar25@gmail.com",
+            Name = "Karlo Peric"
+        },
+        Description = "Ovo je dokumentacija za Edunova API",
+        License = new Microsoft.OpenApi.Models.OpenApiLicense()
+        {
+            Name = "Edukacijska licenca"
+        }
+    };
+    sgo.SwaggerDoc("v1", o);
+    var xmlFile = $"{Assembly.GetExecutingAssembly().GetName().Name}.xml";
+    var xmlPath = Path.Combine(AppContext.BaseDirectory, xmlFile);
+    sgo.IncludeXmlComments(xmlPath, includeControllerXmlComments: true);
+
+});
+// loš način
+builder.Services.AddCors(opcije =>
+{
+    opcije.AddPolicy("CorsPolicy",
+        builder =>
+        builder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader());
+});
 // dodavanje baze podataka
+
 builder.Services.AddDbContext<EdunovaContext>(o =>
     o.UseSqlServer(
         builder.Configuration.
@@ -20,17 +52,31 @@ builder.Services.AddDbContext<EdunovaContext>(o =>
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
+//if (app.Environment.IsDevelopment())
+//{
+app.UseSwagger(opcije =>
 {
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
+    opcije.SerializeAsV2 = true;
+});
+    
+    app.UseSwaggerUI(opcije =>
+    {
+        opcije.ConfigObject.
+        AdditionalItems.Add("requestSnippetsEnabled", true);
+    });
 
-;
+
+//}
+
 app.UseHttpsRedirection();
 
-app.UseAuthorization();
+
 
 app.MapControllers();
+app.UseStaticFiles();
+app.UseCors("CorsPolicy");
+app.UseDefaultFiles();
+app.UseDeveloperExceptionPage();
+app.MapFallbackToFile("index.html");
 
 app.Run();
