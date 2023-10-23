84468f624fd8975155e1a274374fce629b1ab6c1
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-10-24 01:34:37 +0200

    Update App.js

--- Frontend/01_RWD/instragram-pwa/src/App.js
+++ Frontend/01_RWD/instragram-pwa/src/App.js
@@ -4,7 +4,15 @@ import { BrowserRouter as Router, Routes, Route  } from 'react-router-dom';
 import Izbornik from './components/izbornik.component';
 import Pocetna from './components/pocetna.component';
 import NadzornaPloca from './components/nadzornaploca.component';
-import Smjerovi from './components/smjer/smjerovi.component';
+import Osoba from './components/osoba/osoba.component';
+import DodajOsoba from './components/osoba/dodajOsoba.component';
+import PromjeniOsoba from './components/smjer/promjeniOsoba.component';
+import Komentar from './components/komentar/komentar.component';
+import DodajKomentar from './components/komentar/dodajKomentar.component';
+import PromjeniKomentar from './components/osoba/promjeniKomentar.component';
+import Objava from './components/objava/objava.component';
+import DodajObjava from './components/objava/dodajObjava.component';
+import PromjeniObjava from './components/objava/promjeniObjava.component';
 
 export default function App() {
   return (
@@ -13,7 +21,16 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Pocetna />} />
         <Route path='/nadzornaploca' element={<NadzornaPloca />} />
-        <Route path='/smjerovi' element={<Smjerovi />} />
+        <Route path='/osoba' element={<Osoba />} />
+         <Route path="/osoba/dodaj" element={<DodajOsoba />} />
+        <Route path="/osoba/:sifra" element={<PromjeniOsoba />} />
+        <Route path='/komentar' element={<Komentar />} />
+        <Route path="/komentar/dodaj" element={<DodajKomentar />} />
+        <Route path="/komentar/:sifra" element={<PromjeniKomentar />} />
+        <Route path='/objava' element={<Objava />} />
+        <Route path="/objava/dodaj" element={<DodajObjava />} />
+        <Route path="/objava/:sifra" element={<PromjeniObjava />} />
+
       </Routes>
      
     </Router>
