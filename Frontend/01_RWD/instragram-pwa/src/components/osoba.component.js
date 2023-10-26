537fdeff167969f335d6bed20f863eb4d42991b5
Karlocar <132277252+Karlocar@users.noreply.github.com>
2023-10-26 23:14:33 +0200

    Update osoba.component.js

--- Frontend/01_RWD/instragram-pwa/src/components/osoba.component.js
+++ Frontend/01_RWD/instragram-pwa/src/components/osoba.component.js
@@ -1,108 +1,32 @@
 import React, { Component } from "react";
-import ObjavaDataService from "../../services/objava.service";
-import Container from 'react-bootstrap/Container';
-import Row from 'react-bootstrap/Row';
-import Col from 'react-bootstrap/Col';
-import Card from 'react-bootstrap/Card';
-import Button from 'react-bootstrap/Button';
-import { Link } from "react-router-dom";
-import { FaEdit } from 'react-icons/fa';
-import { FaTrash } from 'react-icons/fa';
-import { Modal } from 'react-bootstrap';
-
-
-export default class Osoba extends Component {
-  constructor(props) {
-    super(props);
-    this.dohvatiOsoba = this.dohvatiOsoba.bind(this);
-
-    this.state = {
-      osoba: [],
-      prikaziModal: false
-    };
-  }
+import { Container, Table } from "react-bootstrap";
 
 
 
-  otvoriModal = () => this.setState({ prikaziModal: true });
-  zatvoriModal = () => this.setState({ prikaziModal: false });
+export default class Osoba extends Component{
 
-  componentDidMount() {
-    this.dohvatiOsoba();
-  }
-  dohvatiOsoba() {
-    OsobaataService.getAll()
-      .then(response => {
-        this.setState({
-          osoba: response.data
-        });
-      })
-      .catch(e => {
-        console.log(e);
-      });
-  }
-
-  async obrisiOsoba(sifra){
-    
-    const odgovor = await OsobaDataService.delete(sifra);
-    if(odgovor.ok){
-     this.dohvatiOsoba();
-    }else{
-     // alert(odgovor.poruka);
-      this.otvoriModal();
+    render () {
+        return (
+            <Container>
+            < a href="/smjerovi/dodaj" className="btn btn success gumb">
+                Dodaj novi smjer
+                </a>
+                <Table striped bordered hover responsive>
+                    <thead>
+                        <tr>
+                        <th> Ime</th>   
+                        <th> Prezime</th>
+                        <th> Datumrodenja</th>
+                        <th> Korisnickoime</th>
+                        <th> Lozinka</th>
+                        <th> Slika</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                        {/* Ovdje će doći podaci s backend-a*/}
+                        </tbody>
+                         </Table>
+                         </Container>
+        );
     }
-    
-   }
-
-  render() {
-    const { osoba} = this.state;
-    return (
-
-    <Container>
-      <a href="/osoba/dodaj" className="btn btn-success gumb">Dodaj novu osobu</a>
-    <Row>
-      { objava && objava.map((p) => (
-           
-           <Col key={p.sifra} sm={12} lg={3} md={3}>
-
-              <Card style={{ width: '18rem' }}>
-                <Card.Body>
-                  <Card.Title>{p.ime{p.prezime}</Card.Title>
-                  <Card.Text>
-                    {p.lozinka}
-                  </Card.Text>
-                  <Row>
-                      <Col>
-                      <Link className="btn btn-primary gumb" to={`/osoba/${p.sifra}`}><FaEdit /></Link>
-                      </Col>
-                      <Col>
-                      <Button variant="danger" className="gumb"  onClick={() => this.obrisiOsoba(p.sifra)}><FaTrash /></Button>
-                      </Col>
-                    </Row>
-                </Card.Body>
-              </Card>
-            </Col>
-          ))
-      }
-      </Row>
-
-
-      <Modal show={this.state.prikaziModal} onHide={this.zatvoriModal}>
-              <Modal.Header closeButton>
-                <Modal.Title>Greška prilikom brisanja</Modal.Title>
-              </Modal.Header>
-              <Modal.Body>Osoba se nalazi na jednoj ili više objava i ne može se obrisati.</Modal.Body>
-              <Modal.Footer>
-                <Button variant="secondary" onClick={this.zatvoriModal}>
-                  Zatvori
-                </Button>
-              </Modal.Footer>
-            </Modal>
-
-    </Container>
-
-
-    );
-    
-        }
 }
